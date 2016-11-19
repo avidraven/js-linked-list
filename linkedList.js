d548604e546a2b897b7bb730c989c95e76346b10
David Craven <davidcraven@Davids-MacBook-Pro-2.local>
2016-11-18 23:03:56 -1000

    got all but insert

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,108 @@
  */
 function linkedListGenerator(){
 
+  var head = null;
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail(){
+    var currentNode = getHead();
+    while (currentNode !== null) {
+      if (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }else{
+        return currentNode;
+      }
+    }
+    return currentNode;
+  }
+
+  function add(value){
+    var tail = getTail();
+    var newNode = {
+      value: value,
+      next: null
+    };
+    if (tail === null) {  //checking for empty list, could be head or tail
+      head = newNode;
+    }else{
+      tail.next = newNode;
+    }
+    return newNode; //not neeeded just to add
+  }
+
+  function get(number){
+    var counter = 0;
+    var currentNode = getHead();
+    while (counter < number) {
+      if (currentNode.next !== null) {
+        counter++;
+        currentNode = currentNode.next;
+      }else{
+        return false;
+      }
+    }
+    return currentNode;
+  }
+
+  function remove(number){
+    var counter = 0;
+    var currentNode = getHead();
+    var prevNode = null;
+    if (number === 0) {
+      head = currentNode.next;
+      currentNode = null;
+      return currentNode;
+    }
+    while (counter < number) {
+      if (currentNode.next !== null) {
+        counter++;
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+      }else{
+        return false;
+      }
+    }
+    prevNode.next = currentNode.next;
+    currentNode = null;
+  }
+
+//still fails npm test
+  function insert(value,number){
+    var counter = 0;
+    var currentNode = getHead();
+    var tail = getTail();
+    var prevNode = null;
+    var newNode = {
+      value: value,
+      next: null
+    };
+    if (number === 0) {
+      head = newNode;
+      head.next = currentNode;
+      return currentNode;
+    }
+    while (counter < number) {
+      if (currentNode.next !== null) {
+        counter++;
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+      }else{
+        return false;
+      }
+    }
+    prevNode.next = newNode;
+    newNode.next = currentNode;
+  }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    get: get,
+    remove: remove,
+    insert: insert
+  };
 }
