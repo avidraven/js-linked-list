51b041fc54816ea55bc33cef1b8c41a0260e59b1
David Craven <davidcraven@Davids-MacBook-Pro-2.local>
2016-11-19 13:36:29 -1000

    first time

--- linkedList.js
+++ linkedList.js
@@ -38,11 +38,11 @@ function linkedListGenerator(){
   }
 
   function get(number){
-    var counter = 0;
+    var length = 0;
     var currentNode = getHead();
-    while (counter < number) {
+    while (length < number) {
       if (currentNode.next !== null) {
-        counter++;
+        length++;
         currentNode = currentNode.next;
       }else{
         return false;
@@ -52,7 +52,7 @@ function linkedListGenerator(){
   }
 
   function remove(number){
-    var counter = 0;
+    var length = 0;
     var currentNode = getHead();
     var prevNode = null;
     if (number === 0) {
@@ -60,9 +60,9 @@ function linkedListGenerator(){
       currentNode = null;
       return currentNode;
     }
-    while (counter < number) {
+    while (length < number) {
       if (currentNode.next !== null) {
-        counter++;
+        length++;
         prevNode = currentNode;
         currentNode = currentNode.next;
       }else{
@@ -73,33 +73,67 @@ function linkedListGenerator(){
     currentNode = null;
   }
 
-//still fails npm test
+  function _getLength() {
+    let length = 0;
+    let node = head;
+    if (head === null) {
+      return 0;
+    }else{
+      length = 1;
+    }
+    while (node.next !== null) {
+      length++;
+      node = node.next;
+    }
+    return length;
+  }
+
   function insert(value,number){
-    var counter = 0;
-    var currentNode = getHead();
-    var tail = getTail();
-    var prevNode = null;
     var newNode = {
       value: value,
       next: null
-    };
-    if (number === 0) {
+      };
+    if (number < 0 ||number > _getLength()) {
+      return false;
+    }else if (number === 0) {
+      let nextNode = getHead();
+      newNode.next = nextNode;
       head = newNode;
-      head.next = currentNode;
-      return currentNode;
+    }else{
+      let prevNode = get(number - 1);
+      let nextNode = prevNode.next;
+      prevNode.next = newNode;
+      newNode.next = nextNode;
     }
-    while (counter < number) {
-      if (currentNode.next !== null) {
-        counter++;
-        prevNode = currentNode;
-        currentNode = currentNode.next;
-      }else{
-        return false;
-      }
-    }
-    prevNode.next = newNode;
-    newNode.next = currentNode;
   }
+
+
+ //still fails npm test
+  //  function insert(value,number){
+  //   var length = 0;
+  //   var currentNode = getHead();
+  //   var prevNode = null;
+  //   var newNode = {
+  //     value: value,
+  //     next: null
+  //   };
+  //   if (number === 0) {
+  //     head = newNode;
+  //     head.next = currentNode;
+  //     return currentNode;
+  //   }
+  //   while (length < number) {
+  //     if (currentNode.next !== null) {
+  //       length++;
+  //       prevNode = currentNode;
+  //       currentNode = currentNode.next;
+  //     }else{
+  //       return false;
+  //     }
+  //   }
+  //   prevNode.next = newNode;
+  //   newNode.next = currentNode;
+  // }
 
   return {
     getHead: getHead,
